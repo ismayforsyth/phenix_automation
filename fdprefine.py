11f056214da1d210ffe91b5c7861cbedd84939d4
co2e14 <christian.orr@diamond.ac.uk>
2024-06-06 13:44:12 +0100

    make class

--- fdprefine.py
+++ fdprefine.py
@@ -14,55 +14,57 @@ import plotly.graph_objects as go
 import plotly.io as pio
 
 from multiprocessing import Pool
-cpus = os.cpu_count() - 1
-pool = Pool(cpus)
+class refinefdoubleprime():
+  def __init__(self):
+    cpus = os.cpu_count() - 1
+    pool = Pool(cpus)
 
-try:
-	subprocess.run(["phenix.about"])
-	print("Found Phenix installation")
-except:
-	print("Cannot find Phenix installation. Try to run module load phenix")
+    try:
+      subprocess.run(["phenix.about"])
+      print("Found Phenix installation")
+    except:
+      print("Cannot find Phenix installation. Try to run module load phenix")
 
-mtzIn = input("File location for MTZ: ")
-pdbIn = input("File location for PDB: ")
-projIn = input("Name of project: ")
-genMonomerLib = input("Do you have ligands in the PDB file? (y/n) ").lower()
-if genMonomerLib == "y":
-	print("Generating monomer library, this should only take a few minutes...\n")
-	subprocess.run(["phenix.ready_set", f"{pdbIn}"])
-	pdbInBase, pdbInExt = pdbIn.rsplit('.', 1)
-	pdbInBase = os.path.basename(pdbInBase)
-	ligandIn = str(pdbInBase + ".ligands.cif")
-else:
-	ligandIn = str(None)
-# effIn = input("File location for EFF: ")
-# pdbIn = "/dls/i23/data/2023/cm33851-4/processing/Ismay/Lysozyme/Phenix4/phaser_1/Lysozyme-FinalLSvsnLS_phaser.1.pdb"
-# seqIn = "/dls/i23/data/2023/cm33851-4/processing/Ismay/Lysozyme/Lysozyme.seq"
-# projIn = "Lysozyme"
-# mtzIn = "/dls/i23/data/2023/cm33851-4/processing/Ismay/Lysozyme/New_data_Clonly/LS/8keV/DataFiles/AUTOMATIC_DEFAULT_free.mtz"
+    mtzIn = input("File location for MTZ: ")
+    pdbIn = input("File location for PDB: ")
+    projIn = input("Name of project: ")
+    genMonomerLib = input("Do you have ligands in the PDB file? (y/n) ").lower()
+    if genMonomerLib == "y":
+      print("Generating monomer library, this should only take a few minutes...\n")
+      subprocess.run(["phenix.ready_set", f"{pdbIn}"])
+      pdbInBase, pdbInExt = pdbIn.rsplit('.', 1)
+      pdbInBase = os.path.basename(pdbInBase)
+      ligandIn = str(pdbInBase + ".ligands.cif")
+    else:
+      ligandIn = str(None)
+    # effIn = input("File location for EFF: ")
+    # pdbIn = "/dls/i23/data/2023/cm33851-4/processing/Ismay/Lysozyme/Phenix4/phaser_1/Lysozyme-FinalLSvsnLS_phaser.1.pdb"
+    # seqIn = "/dls/i23/data/2023/cm33851-4/processing/Ismay/Lysozyme/Lysozyme.seq"
+    # projIn = "Lysozyme"
+    # mtzIn = "/dls/i23/data/2023/cm33851-4/processing/Ismay/Lysozyme/New_data_Clonly/LS/8keV/DataFiles/AUTOMATIC_DEFAULT_free.mtz"
 
-elementsToTry = input("Which elements to try, comma separated: ")
-elements = [x.strip() for x in elementsToTry.split(',')]
+    elementsToTry = input("Which elements to try, comma separated: ")
+    elements = [x.strip() for x in elementsToTry.split(',')]
 
-mtzInfo = reflection_file_reader.any_reflection_file(mtzIn)
-millerArray = mtzInfo.as_miller_arrays()
-mtzInfo.file_content()
-mtzobj = iotbx.mtz.object(mtzIn)
-space_group = mtzobj.space_group_name()
-csym = mtzobj.crystals()[0].crystal_symmetry()
-unit_cell = csym.unit_cell()
-unit_cell_strip = (str(unit_cell)).strip('()').replace(',', '')
-mtz = gemmi.read_mtz_file(mtzIn)
-WV = (mtz.dataset(1).wavelength)
-  
-def wavelength_to_eV(wavelength):
+    mtzInfo = reflection_file_reader.any_reflection_file(mtzIn)
+    millerArray = mtzInfo.as_miller_arrays()
+    mtzInfo.file_content()
+    mtzobj = iotbx.mtz.object(mtzIn)
+    space_group = mtzobj.space_group_name()
+    csym = mtzobj.crystals()[0].crystal_symmetry()
+    unit_cell = csym.unit_cell()
+    unit_cell_strip = (str(unit_cell)).strip('()').replace(',', '')
+    mtz = gemmi.read_mtz_file(mtzIn)
+    WV = (mtz.dataset(1).wavelength)
+    
+  def wavelength_to_eV(wavelength):
     h = 6.62607015e-34
     c = 2.99792458e8
     eV = 1.602176634e-19
     energyeV = (h * c) / (float(wavelength) * 1e-10) / eV
     return energyeV
 
-def lookup_fprime(energy, element):
+  def lookup_fprime(energy, element):
     lookupPath = os.path.join("lookup", f"{element}.dat")
     closestEnergy = None
     closestValues = None
@@ -75,92 +77,92 @@ def lookup_fprime(energy, element):
                 closestValues = (fPrime, fDoublePrime)
     return closestValues
 
-def change_elem(pdbPath, elements):
-	pdbInBase, pdbInExt = pdbPath.rsplit('.', 1)
-	pdbInBase = os.path.basename(pdbInBase)
-	print(f"Running on {str(pdbInBase)}")
-	pdbLinesWrite = []
-	pdbList = []
+  def change_elem(pdbPath, elements):
+    pdbInBase, pdbInExt = pdbPath.rsplit('.', 1)
+    pdbInBase = os.path.basename(pdbInBase)
+    print(f"Running on {str(pdbInBase)}")
+    pdbLinesWrite = []
+    pdbList = []
 
-	with open(pdbPath, 'r') as file:
-		pdbLinesWrite = file.readlines()
+    with open(pdbPath, 'r') as file:
+      pdbLinesWrite = file.readlines()
 
-	toChange = []
-	for line in pdbLinesWrite:
-		if line.startswith("HETATM"):
-			print(line.strip())
-			changeHETATM = input("Would you like to run refinement on this HETATM? ")
-			if changeHETATM.lower() in ('y', 'yes'):
-				toChange.append(line)
+    toChange = []
+    for line in pdbLinesWrite:
+      if line.startswith("HETATM"):
+        print(line.strip())
+        changeHETATM = input("Would you like to run refinement on this HETATM? ")
+        if changeHETATM.lower() in ('y', 'yes'):
+          toChange.append(line)
 
-	for element in elements:
-		toFDPRefine = []
-		toWrite = copy.deepcopy(pdbLinesWrite)
-		for i, line in enumerate(pdbLinesWrite):
-			if line in toChange:
-				elementSymbol = element.rjust(2)[:2] if len(element) == 2 else ' ' + element
-				residueName = element.rjust(3)[:3]
-				elementName = element.ljust(4)[:4]
-				lineList = list(line)
-				lineList[12:16] = elementName
-				lineList[17:20] = residueName
-				lineList[76:78] = elementSymbol
-				toWrite[i] = ''.join(lineList)
-				toFDPRefine.append([element, line[21:22].strip(), line[23:26].strip()])
-				
-		with open(f"{pdbInBase}_{element}.{pdbInExt}", 'w') as pdbOut:
-			for line in toWrite:
-				if not line.endswith('\n'):
-					line += '\n'
-				pdbOut.write(line)
-			pdbList.append((f"{pdbInBase}_{element}.{pdbInExt}", element, toFDPRefine))
+    for element in elements:
+      toFDPRefine = []
+      toWrite = copy.deepcopy(pdbLinesWrite)
+      for i, line in enumerate(pdbLinesWrite):
+        if line in toChange:
+          elementSymbol = element.rjust(2)[:2] if len(element) == 2 else ' ' + element
+          residueName = element.rjust(3)[:3]
+          elementName = element.ljust(4)[:4]
+          lineList = list(line)
+          lineList[12:16] = elementName
+          lineList[17:20] = residueName
+          lineList[76:78] = elementSymbol
+          toWrite[i] = ''.join(lineList)
+          toFDPRefine.append([element, line[21:22].strip(), line[23:26].strip()])
+          
+      with open(f"{pdbInBase}_{element}.{pdbInExt}", 'w') as pdbOut:
+        for line in toWrite:
+          if not line.endswith('\n'):
+            line += '\n'
+          pdbOut.write(line)
+        pdbList.append((f"{pdbInBase}_{element}.{pdbInExt}", element, toFDPRefine))
 
-	return pdbList
+    return pdbList
 
-def run_phenix_refine(effFile):
-	subprocess.run(["phenix.refine", effFile, "2>&1", "/dev/null"]) 
- 
-def scrapeLastAnomalousGroupData(log_file_path, theoreticalFDP):
-  with open(log_file_path, 'r') as file:
-    content = file.read()
-
-  last_macro_cycle_index = content.rfind('MACRO_CYCLE')
-
-  content_after_macro_cycle = content[last_macro_cycle_index:]
-    
-  pattern = re.compile(
-      r'Anomalous scatterer group:\s+'
-      r'Selection: "chain (?P<chain>[A-Za-z]) and resid (?P<resid>\d+) and element (?P<element>[A-Za-z]+)"\s+'
-      r'Number of selected scatterers: \d+\s+'
-      r'f_prime: +[+-]?\d+\.\d+\s+'
-      r'f_double_prime: (?P<f_double_prime>[+-]?\d+\.\d+)'
-  )
-
-  matches = pattern.findall(content_after_macro_cycle)
-  data = [(m[0], int(m[1]), m[2], float(m[3]), float(theoreticalFDP), float(abs(float(theoreticalFDP) - (float(m[3]))))) for m in matches]
-
-  return data
-
-def makeTable(scrapedData):
-  header = ['Chain', 'ResidID', 'Element', 'Refined FDP', "Theoretical FDP", "Absolute Difference"]
-  flattenedData = [item for sublist in scrapedData for item in sublist]
-  tableData = list(map(list, zip(*flattenedData)))
+  def run_phenix_refine(effFile):
+    subprocess.run(["phenix.refine", effFile, "2>&1", "/dev/null"]) 
   
-  fig = go.Figure(data=[go.Table(
-    header=dict(values=header,
-                fill_color='paleturquoise',
-                align='left'),
-    cells=dict(values=tableData,
-               fill_color='lavender',
-               align='left'))
-	])
-  pio.write_html(fig, f'{projIn}.html')
+  def scrapeLastAnomalousGroupData(log_file_path, theoreticalFDP):
+    with open(log_file_path, 'r') as file:
+      content = file.read()
+
+    last_macro_cycle_index = content.rfind('MACRO_CYCLE')
+
+    content_after_macro_cycle = content[last_macro_cycle_index:]
+      
+    pattern = re.compile(
+        r'Anomalous scatterer group:\s+'
+        r'Selection: "chain (?P<chain>[A-Za-z]) and resid (?P<resid>\d+) and element (?P<element>[A-Za-z]+)"\s+'
+        r'Number of selected scatterers: \d+\s+'
+        r'f_prime: +[+-]?\d+\.\d+\s+'
+        r'f_double_prime: (?P<f_double_prime>[+-]?\d+\.\d+)'
+    )
+
+    matches = pattern.findall(content_after_macro_cycle)
+    data = [(m[0], int(m[1]), m[2], float(m[3]), float(theoreticalFDP), float(abs(float(theoreticalFDP) - (float(m[3]))))) for m in matches]
+
+    return data
+
+  def makeTable(scrapedData):
+    header = ['Chain', 'ResidID', 'Element', 'Refined FDP', "Theoretical FDP", "Absolute Difference"]
+    flattenedData = [item for sublist in scrapedData for item in sublist]
+    tableData = list(map(list, zip(*flattenedData)))
+    
+    fig = go.Figure(data=[go.Table(
+      header=dict(values=header,
+                  fill_color='paleturquoise',
+                  align='left'),
+      cells=dict(values=tableData,
+                fill_color='lavender',
+                align='left'))
+    ])
+    pio.write_html(fig, f'{projIn}.html')
 
 
-# Create bpos eff file
-def runBPos(pdbIn, elementIn):
-  with open(f'bposEffParam_{elementIn}.eff', 'w') as file:
-    file.write(f'''refinement {{
+  # Create bpos eff file
+  def runBPos(pdbIn, elementIn):
+    with open(f'bposEffParam_{elementIn}.eff', 'w') as file:
+      file.write(f'''refinement {{
       crystal_symmetry {{  
         unit_cell = {unit_cell_strip}
         space_group = {space_group}
@@ -220,23 +222,23 @@ def runBPos(pdbIn, elementIn):
         wavelength = {WV}
         nproc = {cpus}
       }}   
-    }}''')
+      }}''')
 
-  subprocess.run(["phenix.refine", f"bposEffParam_{elementIn}.eff"])
+    subprocess.run(["phenix.refine", f"bposEffParam_{elementIn}.eff"])
 
-def runFdp(elementIn, fPrime, toFDPRefine):
-  anomalousScatterersStr = "anomalous_scatterers {\n"
-  for elemID, chainID, residID in toFDPRefine:
-      groupStr = f"""          group {{
-            selection = chain {chainID} and resid {residID} and element {elemID}
-            f_prime = {fPrime}
-            refine = f_prime *f_double_prime
-          }}\n"""
-      anomalousScatterersStr += groupStr
-  anomalousScatterersStr += "        }"
-  
-  with open(f'fdpEffParam_{elementIn}.eff', 'w') as file:
-    file.write(f'''refinement {{  
+  def runFdp(elementIn, fPrime, toFDPRefine):
+    anomalousScatterersStr = "anomalous_scatterers {\n"
+    for elemID, chainID, residID in toFDPRefine:
+        groupStr = f"""          group {{
+              selection = chain {chainID} and resid {residID} and element {elemID}
+              f_prime = {fPrime}
+              refine = f_prime *f_double_prime
+            }}\n"""
+        anomalousScatterersStr += groupStr
+    anomalousScatterersStr += "        }"
+    
+    with open(f'fdpEffParam_{elementIn}.eff', 'w') as file:
+      file.write(f'''refinement {{  
 			crystal_symmetry {{
         unit_cell = {unit_cell_strip}
         space_group = {space_group}
@@ -296,9 +298,9 @@ def runFdp(elementIn, fPrime, toFDPRefine):
         number_of_macro_cycles = 5  
         wavelength = {WV}
       }}  
-    }}''')
-	
-  subprocess.run(["phenix.refine", f"fdpEffParam_{elementIn}.eff"])  
+      }}''')
+    
+    subprocess.run(["phenix.refine", f"fdpEffParam_{elementIn}.eff"])  
 
 if __name__ == "__main__":    
 	pdbList = change_elem(pdbIn, elements)
